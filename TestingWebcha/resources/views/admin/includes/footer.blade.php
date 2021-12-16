791dd25a260e055eb7fa82e9fc96177d8e17a09a
Chamil <chamiljay88@gmail.com>
2021-12-16 14:10:52 +0530

    20211-121-16

--- TestingWebcha/resources/views/admin/includes/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="footer">
-            <div class="pull-right">
-                
-            </div>
-            <div>
-                <strong>Copyright</strong> &copy; {{date('Y')}}
-            </div>
-        </div>
-        
--- TestingWebcha/resources/views/admin/includes/rightsidebar.blade.php
+++
@@ -1,4 +0,0 @@
-<div id="right-sidebar">
-            
-        </div>
-    
